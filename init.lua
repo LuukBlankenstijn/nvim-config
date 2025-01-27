b9185d9861163dd906642e2c5fab6da14d1c5869
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-01-27 15:11:05 +0100

    create keymapping for fast leaving of insertmode

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-local path_package = vim.fn.stdpath('data') .. '/site'
-
 require("config.lazy")
 
-vim.keymap.set({"n"}, "<C-h>", ":tabp<CR>", {silent = true})
-vim.keymap.set({"n"}, "<C-l>", ":tabn<CR>", {silent = true})
+vim.keymap.set({ "n" }, "<C-h>", ":tabp<CR>", { silent = true })
+vim.keymap.set({ "n" }, "<C-l>", ":tabn<CR>", { silent = true })
+vim.keymap.set({ "i" }, "jk", "<Esc>", { silent = true })
+vim.keymap.set({ "i" }, "kj", "<Esc>", { silent = true })
 
 vim.opt.number = true
