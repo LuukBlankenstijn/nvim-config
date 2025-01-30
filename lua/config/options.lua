52cbf462b5de99af1135ba19e745b09bd4ef4e2c
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-01-30 21:13:09 +0100

    update tree-sitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- LSP Server to use for PHP.
 -- Set to "intelephense" to use intelephense instead of phpactor.
 vim.g.lazyvim_php_lsp = "intelephense"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
