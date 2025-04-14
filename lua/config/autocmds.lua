ec29eebe30d19b909ecb34eec2fa8f8b87f430cb
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-04-14 11:16:35 +0200

    add react code to custom tab widht function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "javascript", "typescript", "yaml", "json", "html", "css", "lua" },
+	pattern = { "javascript", "typescript", "typescriptreact", "yaml", "json", "html", "css", "lua" },
 	callback = function()
 		vim.opt_local.tabstop = 2
 		vim.opt_local.shiftwidth = 2
