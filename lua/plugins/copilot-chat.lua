c1ca236d163bdfa9a0552730a3a255b97eb1069d
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2025-04-14 13:17:17 +0200

    feature: add copilot-chat

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -6,9 +6,76 @@ return {
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			-- See Configuration section for options
+		keys = {
+			{
+				"<leader>co",
+				function()
+					vim.cmd("CopilotChatToggle")
+				end,
+				desc = "Toggle Copilot Chat window",
+			},
+			{
+				"<leader>chl",
+				function()
+					vim.cmd("CopilotChatLoad")
+				end,
+				desc = "Load Copilot Chat history",
+			},
+			{
+				"<leader>chs",
+				function()
+					vim.cmd("CopilotChatSave")
+				end,
+				desc = "Save Copilot Chat history",
+			},
+			-- Visual-mode keymaps for actions on selected code:
+			{
+				"<leader>ce",
+				function()
+					vim.cmd("CopilotChatExplain")
+				end,
+				mode = "v",
+				desc = "Explain selected code (Copilot Chat)",
+			},
+			{
+				"<leader>cr",
+				function()
+					vim.cmd("CopilotChatReview")
+				end,
+				mode = "v",
+				desc = "Review selected code (Copilot Chat)",
+			},
+			{
+				"<leader>cd",
+				function()
+					vim.cmd("CopilotChatDocs")
+				end,
+				mode = "v",
+				desc = "Generate docstring (Copilot Chat)",
+			},
+			{
+				"<leader>ct",
+				function()
+					vim.cmd("CopilotChatTests")
+				end,
+				mode = "v",
+				desc = "Generate tests (Copilot Chat)",
+			},
 		},
-		-- See Commands section for default commands if you want to lazy load on them
+		opts = function()
+			local user = vim.env.USER or "User"
+			user = user:sub(1, 1):upper() .. user:sub(2)
+			return {
+				auto_insert_mode = true,
+				question_header = "  " .. user .. " ",
+				answer_header = "  Copilot ",
+				window = {
+					width = 0.35,
+				},
+				sticky = {
+					"#files",
+				},
+			}
+		end,
 	},
 }
